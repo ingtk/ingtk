544e0d51f88e16b9ac4cfc8e4ee2c0991e8ce5d0
ingtk <ingtk120@gmail.com>
2013-12-10 14:18:45 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 ln -sf ~/dotfiles/_gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
+ln -sf ~/dotfiles/_jshintrc ~/.jshintrc
+ln -sf ~/dotfiles/_zshrc ~/.zshrc
 mkdir ~/.bundle
 
 echo ""
@@ -13,7 +15,7 @@ echo "Plaese install neobundle and vimproc."
 echo "You input following commands and it will work out well."
 echo ""
 echo "Commands:"
-echo "   $ cd .bundle"
+echo "   $ cd ~/.bundle"
 echo "   $ git://github.com/Shougo/neobundle.vim.git" 
 echo "   $ git://github.com/Shougo/vimproc.git"
 echo ""
