6e088dc438d8a505dc8bfeec7db709f350395227
ingtk <tic0102@gmail.com>
2022-11-06 22:14:07 +0900

    update config.fish

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -4,6 +4,7 @@ end
 
 set -x VOLTA_HOME "$HOME/.volta"
 set -x PATH "$VOLTA_HOME/bin" $PATH
+set -x PATH "$HOME/development/flutter/bin" $PATH
 set -Ux fish_user_paths $HOME/.anyenv/bin $fish_user_paths
 
 alias ls exa
@@ -14,6 +15,9 @@ abbr -a cdr "cd (ghq root)/(ghq list | peco)"
 abbr -a push "git push origin (git symbolic-ref --short HEAD)"
 abbr -a pull "git pull origin (git symbolic-ref --short HEAD)"
 abbr -a tigs "tig status"
+abbr -a gco "git checkout"
+abbr -a gf "git fetch"
+abbr -a gfp "git fetch -p"
 
 direnv hook fish | source
 anyenv init - fish | source
---
+++ _config/fish/update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp ~/.config/fish/config.fish ./config.fish
